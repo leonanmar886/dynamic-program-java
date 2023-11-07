57cb9893a7afb6a8159b51f29c6e8843588e5576
Leonan Marques <leonan.marques886@gmail.com>
2023-11-06 22:07:53 -0300

    the sum list is now displayed

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import problems.SumPossible;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {7, 8, 10};
-        boolean n = SumPossible.sumPossible(arr, 15);
+        int[] arr = {7, 6, 4, 10, 61};
+        boolean n = SumPossible.sumPossible(arr, 689);
         System.out.println(n);
     }
 }
--- src/problems/SumPossible.java
+++ src/problems/SumPossible.java
@@ -1,19 +1,25 @@
 package problems;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class SumPossible {
     public static boolean sumPossible(int[] arr, int sum) {
-        return sumPossible(arr, sum, new HashMap<Integer, Boolean>());
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        boolean r = sumPossible(arr, sum, new HashMap<Integer, Boolean>(), list);
+        System.out.println("List: " + list);
+        return r;
     }
 
-    public static boolean sumPossible(int[] arr, int sum, HashMap<Integer, Boolean> map){
+    public static boolean sumPossible(int[] arr, int sum, HashMap<Integer, Boolean> map, List<Integer> list){
         if (sum == 0) return true;
         if (sum < 0) return false;
         if (map.containsKey(sum)) return map.get(sum);
 
         for (int i=0; i < arr.length; i++){
-            if (sumPossible(arr, sum - arr[i], map)){
+            if (sumPossible(arr, sum - arr[i], map, list)) {
+                list.add(arr[i]);
                 map.put(sum, true);
                 return true;
             }
