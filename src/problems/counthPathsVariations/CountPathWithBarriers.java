c3a32c5ef993bdfe7400ea063582f14b6409302f
Leonan Marques <leonan.marques886@gmail.com>
2023-11-12 12:42:05 -0300

    CountPathWithBarriers problem have been solved

---
+++ src/problems/counthPathsVariations/CountPathWithBarriers.java
@@ -0,0 +1,39 @@
+package problems.counthPathsVariations;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class CountPathWithBarriers implements CountPath{
+    @Override
+    public int countPaths(List<List<Integer>> matrix, int x, int y, int destinyX, int destinyY){
+        return countPaths(matrix, x, y, destinyX, destinyY, new HashMap<>());
+    }
+
+    @Override
+    public int countPaths(List<List<Integer>> matrix, int x, int y, int destinyX, int destinyY, HashMap<List<Integer>, Integer> map){
+        if (x == destinyX && y == destinyY){
+            return 1;
+        }
+
+        List<Integer> key = Arrays.asList(x, y);
+
+        if (map.containsKey(key)){
+            return map.get(key);
+        }
+
+        int paths = 0;
+
+        if (x < destinyX && matrix.get(x + 1).get(y) != 1){
+            paths += countPaths(matrix, x + 1, y, destinyX, destinyY, map);
+        }
+        if (y < destinyY && matrix.get(x).get(y + 1) != 1){
+            paths += countPaths(matrix, x, y + 1, destinyX, destinyY, map);
+        }
+
+        map.put(key, paths);
+
+        return paths;
+
+    }
+}
